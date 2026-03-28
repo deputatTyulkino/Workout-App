ad324c4dbf2c371ffbca37c438d47d7bf71ddd05
Tigran Petrosyan <tigr-080807@mail.ru>
2026-03-29 03:01:20 +0500

    Исправление ошибок маршрутов

--- src/constants/routes.const.ts
+++ src/constants/routes.const.ts
@@ -5,8 +5,9 @@ export const ROUTES = {
   register: "/auth/register",
   workouts: "/workouts",
   workout: (id: number) => `/workouts/${id}`,
-  exercises: "/exercises",
-  exercise: (id: number) => `/workouts/${id}`,
+  exercises: (id: number) => `/workouts/${id}/exercises`,
+  exercise: (workout_id: number, exercise_id: number) =>
+    `/workouts/${workout_id}/exercises/${exercise_id}`,
   new_workout: "/create/workout",
   new_exercise: "/create/exercise",
 } as const;
