3349e93cef1a7c75c45c772c9f378195ef86ab65
Tigran Petrosyan <tigr-080807@mail.ru>
2026-03-29 03:02:36 +0500

    Исправление синтаксических ошибок

--- src/api/types/auth.type.ts
+++ src/api/types/auth.type.ts
@@ -1,7 +1,7 @@
 import type { AxiosPromise } from "axios";
 import type { TLogin } from "../../schemas/login.schema";
 import type { TProfile } from "../../schemas/profile.schema";
-import type { TRegister } from "../../schemas/register.shema";
+import type { TRegister } from "../../schemas/register.schema";
 
 type TApiAuthResponse = {
   user: TProfile;
